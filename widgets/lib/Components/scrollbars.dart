61974f7491462600333c0672a00cc37d58f476b6
Karthik-Patchamatla <pkarthikvarma830@gmail.com>
2024-11-04 12:20:13 +0530

    Vertical and Horizantal Scrollbars

---
+++ widgets/lib/Components/scrollbars.dart
@@ -0,0 +1,100 @@
+import 'package:flutter/material.dart';
+
+class ScrollbarExample extends StatelessWidget {
+  const ScrollbarExample({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        
+        body: SafeArea(
+          child: Column(
+            children: [
+              const SizedBox(height: 10),
+              const Text(
+                'Vertical Scrollbar',
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+              ),
+              const SizedBox(height: 10),
+              // Vertical Scrollbar
+              Expanded(
+                child: Scrollbar(
+                  // thumbVisibility: true,
+                  thickness: 5.0,
+                  radius: const Radius.circular(10.0),
+                  interactive: true,
+                  child: ListView(
+                    padding: const EdgeInsets.all(8.0),
+                    children: [
+                      Image.asset(
+                        "images/shoe1.jpg",
+                      ),
+                      const SizedBox(height: 10),
+                      Image.asset(
+                        "images/shoe2.jpg",
+                      ),
+                      const SizedBox(height: 10),
+                      Image.asset(
+                        "images/shoe3.jpg",
+                      ),
+                      const SizedBox(height: 10),
+                      Image.asset(
+                        "images/shoe4.jpg",
+                      ),
+                      const SizedBox(height: 10),
+                      Image.asset(
+                        "images/shoe5.jpg",
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              const SizedBox(height: 20),
+              const Text(
+                'Horizontal Scrollbar',
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+              ),
+              const SizedBox(height: 10),
+              // Horizontal Scrollbar
+              SizedBox(
+                height: 400,
+                child: Scrollbar(
+                  thumbVisibility: true,
+                  thickness: 8.0,
+                  radius: const Radius.circular(10.0),
+                  interactive: true,
+                  child: ListView(
+                    scrollDirection: Axis.horizontal,
+                    padding: const EdgeInsets.all(8.0),
+                    children: [
+                      Image.asset(
+                        "images/shoe6.jpg",
+                      ),
+                      const SizedBox(width: 10),
+                      Image.asset(
+                        "images/shoe7.jpg",
+                      ),
+                      const SizedBox(width: 10),
+                      Image.asset(
+                        "images/shoe8.jpg",
+                      ),
+                      const SizedBox(height: 10),
+                      Image.asset(
+                        "images/shoe9.jpg",
+                      ),
+                      const SizedBox(height: 10),
+                      Image.asset(
+                        "images/shoe10.jpg",
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
--- widgets/lib/main.dart
+++ widgets/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 // import 'package:widgets/Components/container.dart';
-import 'package:widgets/Components/row&col.dart';
+// import 'package:widgets/Components/row&col.dart';
+import 'package:widgets/Components/scrollbars.dart';
 // import 'package:widgets/Components/appBar.dart';
 // import 'package:widgets/Components/bottom_navbar.dart';
 
@@ -14,7 +15,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: RowColWidget(),
+      home: ScrollbarExample(),
     );
   }
 }
